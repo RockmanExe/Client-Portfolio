a5df21bacfa7eba5f316fc75fbab764e6a1601c6
Adalberto May <adalberto-may@lambdaser33smbp.attlocal.net>
2020-06-28 18:55:06 -0400

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Name from '../src/name';
+// import Name from '../src/name';
 // import NavBar from '../src/navbar';
 // import About from '../src/about';
 // import Works from '../src/works';
 // import Photos from '../src/photos';
 // import Thoughts from '../src/thoughts';
-import { Route, Link } from 'react-router-dom';
+// import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
